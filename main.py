c47441fc21bf5e543c0922306d304b385c9b81d0
danipg9 <danixdpg9@gmail.com>
2025-10-04 13:10:39 +0200

    [ADD] crear_usuarios()

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ import random
 
 if __name__ == '__main__':
     main()
+
+
+def main():
+    usuarios = crear_usuarios()
+
+# Función para crear usuarios
+def crear_usuarios(n=15, locale='es_ES'):
+    faker = Faker(locale)
+    usuarios = {}
+    for codigo in range(1, n + 1):
+        usuarios[codigo] = {
+            'nome': faker.name(),
+            'direccion': faker.address().replace('\n', ', '),
+            'correo': faker.email(),
+            'telefono': faker.phone_number()
+        }
+    return usuarios
+
